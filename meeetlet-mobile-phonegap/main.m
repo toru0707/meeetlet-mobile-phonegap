d366225117be042215aa4701d3d243c7e6154379
猪子 徹 <toru0707@inoko-tetsu-no-MacBook-Air.local>
2012-01-08 21:19:37 +0900

    Initial Commit

---
+++ meeetlet-mobile-phonegap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  meeetlet-mobile-phonegap
+//
+//  Created by 猪子 徹 on 12/01/08.
+//  Copyright SCS 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
