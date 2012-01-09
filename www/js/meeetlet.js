cdb02afbe30e08d9d80f92c77c0db08d0c099da5
Toru Inoko <toru0707@gmail.com>
2012-01-09 20:57:36 +0900

    change index.html

--- www/js/meeetlet.js
+++ www/js/meeetlet.js
@@ -1,25 +1,69 @@
-LANG = {
-	footer : {
-		nav : {
-			adjstedEvents : "調整剤のイベント",
-			adjstingEvents : "調整中のイベント",
-			publicEvents : "公開中のイベント",
-			others : "その他"
-		}
-	},
-	header : {
-		title : {
-			default : "meeetlet!",
-			createEvnet : "イベントの作成",
-			adjstedEvents : "調整済みのイベント",
-			adjstingEvents : "調整中のイベント",
-			publicEvents : "公開中のイベント",
-			others : "その他",
-			showInvitePeople : "誘う人",
-			showEventDetail : "イベント詳細",
-			searchEvents : "絞り込み検索",
-			adjastEvent : "イベント調整"
-		}
-	}
+function meeetlet(){
+	this.initialize.apply(this, arguments);
 }
 
+meeetlet.prototype = {
+	initialize : function(host, port, user, password){
+		this.host = host;
+		this.port = port;
+		this.user = user;
+		this.password = password;
+
+	},
+
+	getMe : function(callback){
+		var data = { 'service' : 'get_me'};
+		$.get("http://" + host + ":" + port.to_s() + "/me/get", data, callback);
+	}, 
+
+	getMyFriends : function(callback){
+		var data = { 'service' : 'get_my_friends'};
+		$.get("http://" + host + ":" + port.to_s() + "/me/get", data, callback);
+	}, 
+
+	getMyEvents : function(offset, limit, callback){
+		var data = {'service' : 'get_my_events', 'offset' : offset, 'limit' : limit};
+		$.get("http://" + host + ":" + port.to_s() + "/event/get", data, callback);
+	}, 
+
+	createPreEvent : function(title, dates, places, budgets, genres, participants, comment, callback){
+		var data = {'service' : 'create_pre_event', 'title' : title, 'dates' : dates, 
+			'places' : places, 'budgets' : budgets, 'genres' : genres, 'participants' : participants, 'comment' : comment, 'callback' : callback};
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	
+	}, 
+
+	createEvent : function(title,date, place, budget, genre, participants, comment, callback){
+		var data = {'service' : 'create_event', 'title' : title, 'dates' : dates, 
+			'places' : places, 'budgets' : budgets, 'genres' : genres, 'participants' : participants, 'comment' : comment, 'callback' : callback};		
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	
+	}, 
+   
+	replyPreEvent : function(eventid, dates, places, budgets, genres, comment, callback){
+		var data = {'service' : 'reply_pre_event', 'eventid' : eventid, 'dates' : dates, 
+			'places' : places, 'budgets' : budgets, 'genres' : genres, 'comment' : comment, 'callback' : callback};		
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	
+
+	}, 
+	replyEvent : function(eventid, attend, comment, callback){
+		var data = {'service' : 'reply_event', 'eventid' : eventid, 'attend' : attend, 
+			 'comment' : comment, 'callback' : callback};		
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	
+
+	}, 
+
+	cancelEvent : function(eventid, callback){
+		var data = {'service' : 'cancel_event', 'eventid' : eventid, 'callback' : callback};	
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	}, 
+
+	inviteFriend : function(eventid, users, callback){
+		var data = {'service' : 'invite_friend', 'eventid' : eventid, 'users' : users,  'callback' : callback};	
+		$.get("http://" + host + ":" + port.to_s() + "/event/update", data, callback);
+	
+	}
+
+};
